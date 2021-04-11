c3aaf17a35566719a8114976797680f320043f0c
Gezelligheid.h <plutohh@GezelligheidhdeMacBook-Pro.local>
2021-04-11 22:13:53 +0800

    创建工程

---
+++ src/main/java/cn/gezelligheid/whitelist/ServletInitializer.java
@@ -0,0 +1,13 @@
+package cn.gezelligheid.whitelist;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WhitelistApplication.class);
+    }
+
+}
---
+++ src/test/java/cn/gezelligheid/whitelist/WhitelistApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gezelligheid.whitelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitelistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
